d0be31599d985567ac33fc9c8c24b5078449e40c
mike <masha256bit@protonmail.com>
2022-03-07 14:12:56 -0800

    environment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const WebhookBridge = require('./WebhookBridge');
 const bridge = new WebhookBridge(
     process.env.WEBSOCKET_PROVIDER,
     process.env.WALLET_ADDRESS,
-    process.env.WEBHOOK_ENDPOINT);
+    process.env.WEBHOOK_ENDPOINT,
+    process.env.CASK_ENVIRONMENT);
 
 bridge.run();
