0a08f9858f64305907d569390cb87247c374bddf
mike <masha256bit@protonmail.com>
2022-03-11 18:40:11 -0800

    multi-tenant version of webhook bridge

--- src/WebhookBridge.js
+++ src/WebhookBridge.js
@@ -1,17 +1,46 @@
 const ethers = require('ethers');
 const Web3WsProvider = require('web3-providers-ws');
 const axios = require('axios');
+const { createClient } = require('redis');
 const cask = require('@caskprotocol/sdk');
 
 class WebhookBridge {
 
-    constructor(wssProvider, providerAddress, endpoint, environment='development') {
+    constructor(wssProvider, environment='development') {
         this.wssProvider = wssProvider;
-        this.providerAddress = providerAddress;
-        this.endpoint = endpoint;
         this.environment = environment;
 
+        this.endpointMap = {};
+
         this.chain = cask.core.defaultChains[environment];
+    }
+
+    enableRedis(redisUrl) {
+        this.redis = createClient({url: redisUrl});
+        this.redisKey = process.env.REDIS_PROVIDER_MAP_KEY || 'CaskProviderMap';
+
+        this.redis.on('error', (err) => {
+            console.log('Redis Client Error', err)
+        });
+
+        return this.redis.connect();
+    }
+
+    mapProviderToEndpoint(provider, endpoint) {
+        this.endpointMap[provider] = endpoint;
+    }
+
+    handleError(error) {
+        console.log("Error from web3 subsystem", error);
+        // this.initWeb3(); // reinitialize web3?
+    }
+
+    async getProviderEndpoint(provider) {
+        let endpoint = this.endpointMap[provider];
+        if (!endpoint && this.redis) {
+            endpoint = await this.redis.hGet(this.redisKey, provider);
+        }
+        return endpoint;
     }
 
     initWeb3() {
@@ -29,15 +58,7 @@ class WebhookBridge {
                     onTimeout: true
                 }
             }),
-        )
-
-        let providerAddresses;
-
-        if (this.providerAddress.includes(',')) {
-            providerAddresses = this.providerAddress.split(',');
-        } else {
-            providerAddresses = this.providerAddress;
-        }
+        );
 
         const CaskSubscriptions = new ethers.Contract(
             cask.core.deployments.CaskSubscriptions[this.environment][this.chain],
@@ -48,80 +69,79 @@ class WebhookBridge {
             this.handleError(err);
         });
 
+        if (process.env.VERBOSE) {
+            console.log(`Listening for contract events for providers: ${this.providerAddresses}`);
+        }
+
         CaskSubscriptions.on(
-            CaskSubscriptions.filters.SubscriptionCreated(null, providerAddresses),
+            CaskSubscriptions.filters.SubscriptionCreated(null, this.providerAddresses),
             (consumer, provider, subscriptionId, ref, planId, discountId, event) => {
                 this.handleSubscriptionCreated(consumer, provider, subscriptionId, ref, planId, discountId, event);
             });
 
         CaskSubscriptions.on(
-            CaskSubscriptions.filters.SubscriptionChangedPlan(null, providerAddresses),
+            CaskSubscriptions.filters.SubscriptionChangedPlan(null, this.providerAddresses),
             (consumer, provider, subscriptionId, ref, prevPlanId, planId, discountId, event) => {
                 this.handleSubscriptionChangedPlan(consumer, provider, subscriptionId, ref, prevPlanId,
                     planId, discountId, event);
             });
 
         CaskSubscriptions.on(
-            CaskSubscriptions.filters.SubscriptionPendingChangePlan(null, providerAddresses),
+            CaskSubscriptions.filters.SubscriptionPendingChangePlan(null, this.providerAddresses),
             (consumer, provider, subscriptionId, ref, prevPlanId, planId, event) => {
                 this.handleSubscriptionPendingChangePlan(consumer, provider, subscriptionId, ref, prevPlanId,
                     planId, event);
             });
 
         CaskSubscriptions.on(
-            CaskSubscriptions.filters.SubscriptionChangedDiscount(null, providerAddresses),
+            CaskSubscriptions.filters.SubscriptionChangedDiscount(null, this.providerAddresses),
             (consumer, provider, subscriptionId, ref, planId, discountId, event) => {
                 this.handleSubscriptionChangedDiscount(consumer, provider, subscriptionId, ref, planId,
                     discountId, event);
             });
 
         CaskSubscriptions.on(
-            CaskSubscriptions.filters.SubscriptionPaused(null, providerAddresses),
+            CaskSubscriptions.filters.SubscriptionPaused(null, this.providerAddresses),
             (consumer, provider, subscriptionId, ref, planId, event) => {
                 this.handleSubscriptionPaused(consumer, provider, subscriptionId, ref, planId, event);
             });
 
         CaskSubscriptions.on(
-            CaskSubscriptions.filters.SubscriptionResumed(null, providerAddresses),
+            CaskSubscriptions.filters.SubscriptionResumed(null, this.providerAddresses),
             (consumer, provider, subscriptionId, ref, planId, event) => {
                 this.handleSubscriptionResumed(consumer, provider, subscriptionId, ref, planId, event);
             });
 
         CaskSubscriptions.on(
-            CaskSubscriptions.filters.SubscriptionPendingCancel(null, providerAddresses),
-            (consumer, provider, subscriptionId, ref, planId, event) => {
-                this.handleSubscriptionPendingCancel(consumer, provider, subscriptionId, ref, planId, event);
+            CaskSubscriptions.filters.SubscriptionPendingCancel(null, this.providerAddresses),
+            (consumer, provider, subscriptionId, ref, planId, cancelAt, event) => {
+                this.handleSubscriptionPendingCancel(consumer, provider, subscriptionId, ref, planId, cancelAt, event);
             });
 
         CaskSubscriptions.on(
-            CaskSubscriptions.filters.SubscriptionCanceled(null, providerAddresses),
+            CaskSubscriptions.filters.SubscriptionCanceled(null, this.providerAddresses),
             (consumer, provider, subscriptionId, ref, planId, event) => {
                 this.handleSubscriptionCanceled(consumer, provider, subscriptionId, ref, planId, event);
             });
 
         CaskSubscriptions.on(
-            CaskSubscriptions.filters.SubscriptionRenewed(null, providerAddresses),
+            CaskSubscriptions.filters.SubscriptionRenewed(null, this.providerAddresses),
             (consumer, provider, subscriptionId, ref, planId, discountId, event) => {
                 this.handleSubscriptionRenewed(consumer, provider, subscriptionId, ref, planId, event);
             });
 
         CaskSubscriptions.on(
-            CaskSubscriptions.filters.SubscriptionTrialEnded(null, providerAddresses),
+            CaskSubscriptions.filters.SubscriptionTrialEnded(null, this.providerAddresses),
             (consumer, provider, subscriptionId, ref, planId, discountId, event) => {
                 this.handleSubscriptionTrialEnded(consumer, provider, subscriptionId, ref, planId, event);
             });
 
         CaskSubscriptions.on(
-            CaskSubscriptions.filters.SubscriptionPastDue(null, providerAddresses),
+            CaskSubscriptions.filters.SubscriptionPastDue(null, this.providerAddresses),
             (consumer, provider, subscriptionId, ref, planId, discountId, event) => {
                 this.handleSubscriptionPastDue(consumer, provider, subscriptionId, ref, planId, event);
             });
 
-    }
-
-    async handleError(error) {
-        console.log("Error from web3 subsystem", error);
-        // this.initWeb3(); // reinitialize web3?
     }
 
     async handleSubscriptionCreated(consumer, provider, subscriptionId, ref, planId, discountId, event) {
@@ -130,7 +150,12 @@ class WebhookBridge {
             {consumer, provider, subscriptionId: subscriptionId.toHexString(), ref, planId, discountId},
             event
         );
-        this.sendWebhook(payload);
+        const endpoint = await this.getProviderEndpoint(provider);
+        if (endpoint) {
+            this.sendWebhook(endpoint, payload);
+        } else {
+            console.log(`No endpoint mapped for provider ${provider}`);
+        }
     }
 
     async handleSubscriptionChangedPlan(consumer, provider, subscriptionId, ref, prevPlanId,
@@ -141,7 +166,12 @@ class WebhookBridge {
             {consumer, provider, subscriptionId: subscriptionId.toHexString(), ref, prevPlanId, planId, discountId},
             event
         );
-        this.sendWebhook(payload);
+        const endpoint = await this.getProviderEndpoint(provider);
+        if (endpoint) {
+            this.sendWebhook(endpoint, payload);
+        } else {
+            console.log(`No endpoint mapped for provider ${provider}`);
+        }
     }
 
     async handleSubscriptionPendingChangePlan(consumer, provider, subscriptionId, ref, prevPlanId, planId, event) {
@@ -150,7 +180,12 @@ class WebhookBridge {
             {consumer, provider, subscriptionId: subscriptionId.toHexString(), ref, prevPlanId, planId},
             event
         );
-        this.sendWebhook(payload);
+        const endpoint = await this.getProviderEndpoint(provider);
+        if (endpoint) {
+            this.sendWebhook(endpoint, payload);
+        } else {
+            console.log(`No endpoint mapped for provider ${provider}`);
+        }
     }
 
     async handleSubscriptionChangedDiscount(consumer, provider, subscriptionId, ref, planId, discountId, event) {
@@ -159,7 +194,12 @@ class WebhookBridge {
             {consumer, provider, subscriptionId: subscriptionId.toHexString(), ref, planId, discountId},
             event
         );
-        this.sendWebhook(payload);
+        const endpoint = await this.getProviderEndpoint(provider);
+        if (endpoint) {
+            this.sendWebhook(endpoint, payload);
+        } else {
+            console.log(`No endpoint mapped for provider ${provider}`);
+        }
     }
 
     async handleSubscriptionPaused(consumer, provider, subscriptionId, ref, planId, event) {
@@ -168,7 +208,12 @@ class WebhookBridge {
             {consumer, provider, subscriptionId: subscriptionId.toHexString(), ref, planId},
             event
         );
-        this.sendWebhook(payload);
+        const endpoint = await this.getProviderEndpoint(provider);
+        if (endpoint) {
+            this.sendWebhook(endpoint, payload);
+        } else {
+            console.log(`No endpoint mapped for provider ${provider}`);
+        }
     }
 
     async handleSubscriptionResumed(consumer, provider, subscriptionId, ref, planId, event) {
@@ -177,16 +222,26 @@ class WebhookBridge {
             {consumer, provider, subscriptionId: subscriptionId.toHexString(), ref, planId},
             event
         );
-        this.sendWebhook(payload);
+        const endpoint = await this.getProviderEndpoint(provider);
+        if (endpoint) {
+            this.sendWebhook(endpoint, payload);
+        } else {
+            console.log(`No endpoint mapped for provider ${provider}`);
+        }
     }
 
-    async handleSubscriptionPendingCancel(consumer, provider, subscriptionId, ref, planId, event) {
+    async handleSubscriptionPendingCancel(consumer, provider, subscriptionId, ref, planId, cancelAt, event) {
         const payload = await this.webhookPayload(
             event.event,
             {consumer, provider, subscriptionId: subscriptionId.toHexString(), ref, planId},
             event
         );
-        this.sendWebhook(payload);
+        const endpoint = await this.getProviderEndpoint(provider);
+        if (endpoint) {
+            this.sendWebhook(endpoint, payload);
+        } else {
+            console.log(`No endpoint mapped for provider ${provider}`);
+        }
     }
 
     async handleSubscriptionCanceled(consumer, provider, subscriptionId, ref, planId, event) {
@@ -195,7 +250,12 @@ class WebhookBridge {
             {consumer, provider, subscriptionId: subscriptionId.toHexString(), ref, planId},
             event
         );
-        this.sendWebhook(payload);
+        const endpoint = await this.getProviderEndpoint(provider);
+        if (endpoint) {
+            this.sendWebhook(endpoint, payload);
+        } else {
+            console.log(`No endpoint mapped for provider ${provider}`);
+        }
     }
 
     async handleSubscriptionRenewed(consumer, provider, subscriptionId, ref, planId, event) {
@@ -204,7 +264,12 @@ class WebhookBridge {
             {consumer, provider, subscriptionId: subscriptionId.toHexString(), ref, planId},
             event
         );
-        this.sendWebhook(payload);
+        const endpoint = await this.getProviderEndpoint(provider);
+        if (endpoint) {
+            this.sendWebhook(endpoint, payload);
+        } else {
+            console.log(`No endpoint mapped for provider ${provider}`);
+        }
     }
 
     async handleSubscriptionTrialEnded(consumer, provider, subscriptionId, ref, planId, event) {
@@ -213,7 +278,12 @@ class WebhookBridge {
             {consumer, provider, subscriptionId: subscriptionId.toHexString(), ref, planId},
             event
         );
-        this.sendWebhook(payload);
+        const endpoint = await this.getProviderEndpoint(provider);
+        if (endpoint) {
+            this.sendWebhook(endpoint, payload);
+        } else {
+            console.log(`No endpoint mapped for provider ${provider}`);
+        }
     }
 
     async handleSubscriptionPastDue(consumer, provider, subscriptionId, ref, planId, event) {
@@ -222,9 +292,13 @@ class WebhookBridge {
             {consumer, provider, subscriptionId: subscriptionId.toHexString(), ref, planId},
             event
         );
-        this.sendWebhook(payload);
+        const endpoint = await this.getProviderEndpoint(provider);
+        if (endpoint) {
+            this.sendWebhook(endpoint, payload);
+        } else {
+            console.log(`No endpoint mapped for provider ${provider}`);
+        }
     }
-
 
     async webhookPayload(name, args, blockchainEvent) {
         const block = await blockchainEvent.getBlock();
@@ -244,18 +318,18 @@ class WebhookBridge {
         }
     }
 
-    async sendWebhook(payload) {
+    async sendWebhook(endpoint, payload) {
         try {
             if (process.env.VERBOSE) {
-                console.log(`Sending webhook for event ${payload.event}`);
+                console.log(`Sending webhook to endpoint ${endpoint} for event ${payload.event}`);
             }
-            const response = await axios.post(this.endpoint, payload);
+            const response = await axios.post(endpoint, payload);
             if (response.status >= 200 && response.status < 400) {
                 if (process.env.VERBOSE) {
                     console.log(`Successful webhook post`);
                 }
             } else {
-                console.log(`Error from remote endpoint: ${response.status}`);
+                console.log(`Error from remote endpoint ${endpoint}: ${response.status}`);
             }
         } catch (e) {
             console.log(`Error sending webhook: ${e.message}`);
@@ -263,8 +337,37 @@ class WebhookBridge {
     }
 
 
-    async run() {
-        console.log(`Starting bridge for environment ${this.environment} (using chain ${this.chain})`);
+    async runSingle(providerAddress, endpoint) {
+        console.log(`Starting webhook bridge for environment ${this.environment} (using chain ${this.chain})`);
+
+        if (providerAddress.includes(',')) {
+            const providerAddresses = providerAddress.split(',');
+            providerAddresses.forEach((provider) => {
+                this.mapProviderToEndpoint(provider, endpoint);
+            });
+            this.providerAddresses = providerAddresses;
+        } else {
+            this.mapProviderToEndpoint(providerAddress, endpoint);
+            this.providerAddresses = providerAddress;
+        }
+
+        this.initWeb3();
+    }
+
+    async runMulti(redisUrl) {
+        console.log(`Starting multi-tenant webhook bridge for environment ${this.environment} (using chain ${this.chain})`);
+
+        await this.enableRedis(redisUrl);
+
+        const mapping = await this.redis.hGetAll(this.redisKey);
+
+        let providerAddresses = Object.keys(mapping);
+        if (providerAddresses.length === 1) {
+            this.providerAddresses = providerAddresses[0];
+        } else {
+            this.providerAddresses = providerAddresses;
+        }
+
         this.initWeb3();
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 
 const WebhookBridge = require('./WebhookBridge');
 
-const bridge = new WebhookBridge(
-    process.env.WEBSOCKET_PROVIDER,
-    process.env.WALLET_ADDRESS,
-    process.env.WEBHOOK_ENDPOINT,
-    process.env.CASK_ENVIRONMENT);
+const bridge = new WebhookBridge(process.env.WEBSOCKET_PROVIDER, process.env.CASK_ENVIRONMENT);
 
-bridge.run();
+if (process.env.REDIS_URL) {
+    bridge.runMulti(process.env.REDIS_URL);
+} else {
+    bridge.runSingle(process.env.WALLET_ADDRESS, process.env.WEBHOOK_ENDPOINT);
+}
+
+
